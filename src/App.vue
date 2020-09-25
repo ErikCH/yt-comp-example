3c7705b34136e6f0d77e35b851bbef04393cd260
Erik Hanchett <erik.hanchett@gmail.com>
2020-09-24 21:34:08 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col items-center w-2/12 mx-auto">
+    <h3 class="font-medium text-xl">Enter Github User Name</h3>
+    <input
+      v-model="name"
+      class="ml-4 p-1 border-solid shadow-outline mt-4"
+      placeholder="Github User name"
+    />
+    <button
+      class="border-red-700 mt-4 w-32 rounded-md p-2 mb-10 uppercase font-semibold bg-blue-300 color-white border-solid"
+      @click="newName = name"
+    >Press me</button>
+  </div>
+  <ul>
+    <li v-for="lib in data" :key="lib.name">{{lib.name}}</li>
+  </ul>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import { ref, watch, reactive, toRefs } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    const name = ref(null);
+    const newName = ref(null);
+    const state = reactive({ data: [] });
+
+    watch(() => {
+      console.log("newName", newName.value);
+      if (newName.value)
+        fetch(`https://api.github.com/users/${newName.value}/repos`)
+          .then((response) => response.json())
+          .then((data) => {
+            state.data = data;
+            console.log("data", data);
+            name.value = "";
+          });
+    });
+    return {
+      name,
+      newName,
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 
 <style>
